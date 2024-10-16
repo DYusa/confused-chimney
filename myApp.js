6aa07f5aedb457e7322d44cb028a7c04c8724a83
DYusa <yusaeced@gmail.com>
2024-10-16 16:44:47 +0200

    express librarsinin middware i yerine, req res belirterek iletisimin kaynaklarini belirtik, kullaniciya cevabi biz veriyrouz cunku we are the sever

--- myApp.js
+++ myApp.js
@@ -24,4 +24,9 @@ app.use("/public", express.static(styles));
 //   res.express.static(styles);
 // no workoe
 // });
+
+// her mesajda id gerekemedigi icin requesti her seferinde nasil alacagimizi yazmiyoruz
+
+//const message = (req, res) => {}
+app.get("/json", (req, res) => res.json({ message: "Hello json" }));
 module.exports = app;
