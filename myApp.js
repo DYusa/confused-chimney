588195a9be0f1814975cd3669600c8c9cc26501f
DYusa <yusaeced@gmail.com>
2024-10-16 16:07:52 +0200

    app.method(path, handler), here handler is req res and because get requires an absolute path we define it as a construct

--- myApp.js
+++ myApp.js
@@ -4,10 +4,16 @@ let app = express();
 
 console.log("Hello World");
 
-const salutationHandler = (req, res) => {
-  res.send("Hello Express");
-};
+// const salutationHandler = (req, res) => {
+//   res.send("Hello Express");
+// };
 
-app.get("/", salutationHandler);
+// app.get("/", salutationHandler);
+
+const HomePage = __dirname + "/view/index.html";
+
+app.get("/", (req, res) => {
+  res.sendFiles(HomePage);
+});
 
 module.exports = app;
